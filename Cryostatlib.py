5f24172c356bf153d828bfb32db123016882b8e9
Unknown <grigorevvol@gmail.com>
2017-11-13 23:07:55 +0100

    USBGPIB changed but not tested.
    other way of writing commands, dictionaries for otput func and sens

--- Cryostatlib.py
+++ Cryostatlib.py
@@ -10,19 +10,37 @@ import serial
 def Cyostat(object):
     
     def __init__(self):
-        self.temperuture=0
-        self.COMport='COM4'
+        self.temperuture=0              
+        self.ser=serial.Serial()
+        self.ser.port='COM4'
+        self.ser.baudrate=115200
+        
+    def writestring(self, SCPICommands):
+        '''Transform list of SCPI commands to strig for writing to RS-485 port. Adds address of port and seporator, transform it to required format. SCPICommands should be list of strings'''
+        String=''
+        for item in SCPICommands:
+            String=String+item + ':'
+        String=String - ':'+'\n'
+        return String.encode('utf-8')
+    
     
     def connect(self):
-        ser=serial.Serial()
-        ser.port=self.COMport
-
-
-ser=serial.Serial()
-ser.port='COM4'
-ser.baudrate=115200
-ser.open()
-ser.write('*IDN?\n'.encode('utf-8'))
-print(ser.readline())
-ser.write('READ:SYS:TIME\n'.encode('utf-8'))
-print(ser.readline())
+        try:
+            self.ser.open()
+            self.ser.write(self.writestring('*IDN?'))
+            print(self.ser.readline())
+            commands=['READ','SYS','TIME']
+            self.ser.write(self.writestring(commands))
+            print(self.ser.readline())
+            self.ser.close()
+        except: 
+            self.ser.close()
+            
+    def SetTemperature(self, temperature):
+        try:
+            self.ser.open()
+            command=['SET','DEV','MB1.T1','TEMP','TSET',str(temperature)]
+            self.ser.write(self.writesttring(command))
+            self.ser.close()
+        except:
+            self.close
--- USBGPIBlib.py
+++ USBGPIBlib.py
@@ -14,46 +14,61 @@ class USBGPIB(object):
         self.COMPort='COM6'
         self.Baud=115200
         self.deviceAddr=8
+        self.ser=serial.Serial()
+        self.ser.baudrate=self.Baud
+        self.ser.port=self.COMPort
+        self.SensDict={'2nV/fA':0,'5nV/fA':1,'10nV/fA':2,'20nV/fA':3,'50nV/fA':4,'100nV/fA':5,
+        '200nV/fA':6,'500nV/fA':7,'1uV/pA':8,'2uV/pA':9,'5uV/pA':10,'10uV/pA':11,'20uV/pA':12,
+        '50uV/pA':13,'100uV/pA':14,'200uV/pA':15,'500uV/pA':16,'1mV/nA':17,'2mV/nA':18,
+        '5mV/nA':19,'10mV/nA':20,'20mV/nA':21,'50mV/nA':22,'100mV/nA':23,'200mV/nA':24,
+        '500mV/nA':25,'1V/uA':26}
         
     def connect(self):
-        ser=serial.Serial()
-        ser.baudrate=self.Baud
-        ser.port=self.COMPort
         try:    
-            ser.open()
-            ser.write('++ver\r\n'.encode('utf-8'))
-            Value=ser.readline()
+            self.ser.open()
+            self.ser.write('++ver\r\n'.encode('utf-8'))
+            Value=self.ser.readline()
             print(Value)
-            ser.close()
+            self.ser.close()
         except: 
             print('xui')
-            ser.close()    
+            self.ser.close()    
         
     def SendCommand(self,Command):
-        ser=serial.Serial()
-        ser.baudrate=self.Baud
-        ser.port=self.COMPort
         try:
-            ser.open()
-            ser.write((Command+'\r\n').encode('utf-8'))
-            ser.close()
+            self.ser.open()
+            self.ser.write((Command+'\r\n').encode('utf-8'))
+            self.ser.close()
         except: 
             print('xui')
-            ser.close()
+            self.ser.close()
+    
+    def ReadLockIn(self, Command):
+        try:
+            self.ser.open()
+            self.ser.write(Command)
+            self.ser.write(('++read\r\n').encode('utf-8'))
+            Value=self.ser.readline()
+            return Value
+        except:
+            self.ser.close()
             
-    def ReadValue(self,parametr=1):
-        ser=serial.Serial()
-        ser.baudrate=self.Baud
-        ser.port=self.COMPort
-        try:    
-            ser.open()
-            ser.write(('OUTP ? '+str(parametr)+'\r\n').encode('utf-8'))
-          # time.sleep(0.1)
-            ser.write(('++read\r\n').encode('utf-8'))
-           # time.sleep(0.1)
-            Value=ser.readline()
-            print(Value)
-            ser.close()
-        except: 
-            print('xui')
-            ser.close() 
+    def ReadValue(self,parametr):
+        '''parametr is a string like in manual. except Theta'''
+        OutputDict={'X':1,'Y':2,'R':3,'Theta':4,'Aux in 1':5,'Aux in 2':6,'Aux in 3':7,
+                    'Aux in 4':8,'Reference Frequency':9,'CH1 display': 10,'CH2 diplay': 11}
+        Command='OUTP ?' + OutputDict[parametr]
+        Value=self.ReadLockIn(Command)
+        print(Value)
+        
+    def SetToDefault(self):
+        self.SendCommand('*RST')
+    
+    def SetSensitivity(self, sens):
+        '''Sets the sensitivity on SR830 Lock in. sens is string like on the front panel'''
+        command='SENS'+str(self.SensDict[sens])
+        self.SendCommand(command)
+    
+    def GetSensetivity(self):
+        self.SendCommand('SENS ?')
+        
